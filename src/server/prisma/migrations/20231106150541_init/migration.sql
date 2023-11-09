def40fa22874db98caf7bca7d907b2871037be06
mpollard33 <mpollard33@gmail.com>
2023-11-09 11:40:29 -0700

    updated migrations.sql to match schema.prisma Student model

--- src/server/prisma/migrations/20231106150541_init/migration.sql
+++ src/server/prisma/migrations/20231106150541_init/migration.sql
@@ -1,18 +1,9 @@
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Student" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "gpa" REAL NOT NULL
 );
-
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "description" TEXT NOT NULL,
-    "done" BOOLEAN NOT NULL,
-    "userId" INTEGER NOT NULL,
-    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
